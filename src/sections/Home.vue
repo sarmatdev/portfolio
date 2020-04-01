12713763e128d0a634b01586ced38098fb4037c8
Sarmat Khachirov <skhachirov12@gmail.com>
2020-04-01 18:34:08 +0300

    home section

--- src/sections/Home.vue
+++ src/sections/Home.vue
@@ -1,43 +1,34 @@
 <template>
-  <div>
-    <Navbar />
-    <div class="section">
-      <section class="home">
-        <h1 class="home__title">
-          Hello ✌🏻, <br />
-          I am <span>Sarmat</span>
-        </h1>
-        <h2 class="home__sub-title">
-          A <span>self-taught</span> Front-end developer
-        </h2>
-        <g-image
-          class="home__img"
-          src="~/assets/images/srmt.png"
-          alt=""
-        ></g-image>
-      </section>
-    </div>
-  </div>
+  <section class="home">
+      <h1 class="home__title">
+        Hello ✌🏻, <br />
+        I am <span>Sarmat</span>
+      </h1>
+      <h2 class="home__sub-title">
+        A <span>self-taught</span> Front-end developer
+      </h2>
+  </section>
 </template>
 
 <script>
-import Navbar from '~/components/Navbar';
-export default {
-  components: {
-    Navbar
-  }
-};
+export default {};
 </script>
 
 <style lang="scss">
 .home {
   color: var(--color-white);
   position: relative;
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   &__img {
     position: absolute;
     z-index: -1;
-    top: -15%;
+    top: -20%;
     overflow: hidden;
     left: 400px;
     width: 900px;
@@ -48,9 +39,10 @@ export default {
   &__title {
     font-weight: 200;
     font-size: 6rem;
+    text-align: center;
 
     span {
-      font-weight: 600;
+      font-weight: 500;
       color: var(--color-main);
     }
   }
