bc2abd13970d6a38e98baf8b74024b26355cf758
Sarmat Khachirov <skhachirov12@gmail.com>
2020-04-01 18:33:51 +0300

    about section

---
+++ src/sections/About.vue
@@ -0,0 +1,74 @@
+<template>
+  <Layout>
+    <h1 class="title">About me</h1>
+    <h2 class="sup-title">If you’re wondering who I am…</h2>
+    <p class="paragraph">
+      I am <span class="highlight">Sarmat</span>, 20 years old self-taught
+      <span class="highlight">Front-end developer</span>.
+    </p>
+
+    <p class="paragraph">
+      In October 2018 after 1 month university studying I did not agree with the
+      further state of affairs and I decided to quit him and start my
+      <span class="bold">front-end developer journey</span>. Also I developed in
+      other directions, such as <span class="bold">electronics</span>,
+      <span class="bold">3d printing</span>.
+    </p>
+    <p class="paragraph">
+      During this time, I took many online courses on Udemy and other course
+      platforms, read books for developing and also watched ton of YouTube
+      videos about JavaScript and related directions.
+    </p>
+    <p class="paragraph">
+      I also did a few personal <span class="highlight">projects</span> with the
+      knowledge I gathered from the courses, you can check them on my
+      <span class="highlight">portfolio</span>.
+    </p>
+
+    <p class="paragraph">
+      I <span class="highlight">always</span> want to learn more, do more, and
+      <span class="highlight">be</span> more. With constant
+      <span class="highlight">hunger</span>
+      for new challenges and <span class="highlight">learning</span> new things.
+    </p>
+    <p class="paragraph">
+      My current stack of
+      <span class="highlight">languages/technologies</span> is:
+    </p>
+    <p class="stack">
+      HTML5 • CSS3 • JAVASCRIPT • SASS • WEBPACK • BABEL • VUEJS • VUEX •
+      VUE-ROUTER • NUXT • GRIDSOME
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.paragraph {
+  font-size: 2rem;
+  font-weight: 200;
+  color: var(--color-white);
+  text-align: center;
+  padding: 1rem 0;
+}
+
+.highlight {
+  background-color: var(--color-main);
+  font-style: italic;
+  padding: 0.1rem;
+}
+
+.bold {
+  font-weight: 500;
+}
+
+.stack {
+  text-align: center;
+  font-size: 2rem;
+  text-transform: uppercase;
+  font-weight: 500;
+}
+</style>
