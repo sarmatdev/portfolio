c8e19106d106cae3a8aadd711f56d4659a6f4521
Sarmat Khachirov <skhachirov12@gmail.com>
2020-04-02 14:19:49 +0300

    install vue-scrollto

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/styles/style.scss'
+import DefaultLayout from '~/layouts/Default.vue';
+import VueScrollTo from 'vue-scrollto';
+import '~/assets/styles/style.scss';
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
+
+  Vue.use(VueScrollTo, {
+    duration: 500,
+    easing: 'ease'
+  });
 }
