94dc9944ecf56637fc5f2ee9eef56b3925c3151b
Sarmat Khachirov <skhachirov12@gmail.com>
2021-01-29 11:49:26 +0300

    feat: add dist

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,12 +1,6 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
   siteName: 'Sarmat | Front-end Developer',
-  siteUrl: 'https://www.sarmat.dev/',
+  siteUrl: '',
   plugins: [
     {
       use: '@gridsome/source-filesystem',
